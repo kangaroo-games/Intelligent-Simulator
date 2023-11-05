6787db4b1f426275c96a0a413c1bcad9744ed88f
kangaroo-games <138738604+kangaroo-games@users.noreply.github.com>
2023-11-05 00:51:32 -0400

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,21 @@
+var settings = document.getElementById("settings");
+
+function openSettings() {
+		settings.style.display = "block";
+}
+
+function closeSettings() {
+		settings.style.display = "none";
+}
+
+function bgColorW() {
+	document.body.style.backgroundColor = "white";
+}
+
+function bgColorP() {
+	document.body.style.backgroundColor = "pink";
+}
+
+function bgColorR() {
+	document.body.style.backgroundColor = "red";
+}
